a225ab0881abd598c04846533bd4d7ee3c3d59f2
nickmcdowall <nicholas.mcdowall@gmail.com>
2021-06-02 20:23:41 +0100

    #18 Don't close popup when clicking within popup

--- src/main/java/com/lsd/report/model/PopupContent.java
+++ src/main/java/com/lsd/report/model/PopupContent.java
@@ -12,7 +12,7 @@ public class PopupContent {
 
     public static ContainerTag popupDiv(String id, String title, String content) {
         return div().withId(id).withClass("overlay").attr("onclick", "location.href='#!';").with(
-                div().withClass("popup").with(
+                div().withClass("popup").attr("onclick", "event.stopPropagation();").with(
                         h2(title),
                         a().withClass("close").withHref("#!").with(new UnescapedText("&times;")),
                         div().withClass("content").with(
