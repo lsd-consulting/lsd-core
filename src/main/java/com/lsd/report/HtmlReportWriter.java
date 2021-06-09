3a3e7095f1a252aca0886120c7dcb6de41d6a261
nickmcdowall <nicholas.mcdowall@gmail.com>
2021-06-09 22:43:12 +0100

    #9 Add status to scenarios (warn error and success

--- src/main/java/com/lsd/report/HtmlReportWriter.java
+++ src/main/java/com/lsd/report/HtmlReportWriter.java
@@ -6,8 +6,10 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Optional;
 
 import static com.lsd.properties.LsdProperties.OUTPUT_DIR;
 import static org.apache.commons.text.WordUtils.capitalizeFully;
@@ -21,8 +23,28 @@ public class HtmlReportWriter {
         outputDir.mkdir();
         Path path = new File(outputDir, createFileName(report.getTitle())).toPath();
         Files.write(path, new HtmlReportRenderer().render(report).getBytes());
+        copyFileIfMissing("static/style.css", new File(outputDir, "style.css"));
         System.out.println("LSD Report:\nfile://" + path.toAbsolutePath());
         return path;
+    }
+
+    private static void copyFileIfMissing(String resourceName, File targetFile) {
+        if (!targetFile.isFile()) {
+            var classLoader = HtmlReportWriter.class.getClassLoader();
+            var resource = Optional.ofNullable(classLoader.getResource(resourceName));
+            resource.ifPresent(url -> {
+                var sourceFile = new File(url.getFile());
+                copySafely(sourceFile, targetFile);
+            });
+        }
+    }
+
+    private static void copySafely(File sourceFile, File targetFile) {
+        try {
+            Files.copy(sourceFile.toPath(), targetFile.toPath());
+        } catch (IOException e) {
+            System.err.println("Failed to copy file from source to target: " + sourceFile + ", target: " + targetFile);
+        }
     }
 
     private static String createFileName(String title) {
