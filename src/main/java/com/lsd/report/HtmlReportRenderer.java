6b3c266a8c9926bf4570e993d567582343d87f41
lukasz.gryzbon <lukasz.gryzbon@integreety.co.uk>
2022-03-15 19:31:25 +0000

    chore: Move html-report page rendering to handlebars - sanitising added
    #56 Replacing pebble templates to allow upgrading java version

--- src/main/java/com/lsd/report/HtmlReportRenderer.java
+++ src/main/java/com/lsd/report/HtmlReportRenderer.java
@@ -2,33 +2,27 @@ package com.lsd.report;
 
 import com.github.jknack.handlebars.Context;
 import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Helper;
 import com.github.jknack.handlebars.Template;
 import com.github.jknack.handlebars.context.FieldValueResolver;
 import com.github.jknack.handlebars.context.JavaBeanValueResolver;
 import com.github.jknack.handlebars.context.MapValueResolver;
 import com.github.jknack.handlebars.context.MethodValueResolver;
 import com.lsd.report.model.Report;
-import com.lsd.report.pebble.LsdPebbleExtension;
-import com.mitchellbosecke.pebble.PebbleEngine;
 import lombok.SneakyThrows;
 
 import java.util.Map;
 
+import static com.lsd.Sanitiser.sanitise;
+
 
 public class HtmlReportRenderer implements ReportRenderer {
-
-    private final PebbleEngine engine = new PebbleEngine.Builder()
-            .extension(new LsdPebbleExtension())
-            .build();
 
     private final Template template = compileTemplate("templates/html-report");
 
     @SneakyThrows
     @Override
     public String render(Report report) {
-
-//        String uml = report.getScenarios().iterator().next().getSequenceDiagram().getUml();
-
         Context context = Context.newBuilder(Map.of("report", report))
                 .resolver(MapValueResolver.INSTANCE, JavaBeanValueResolver.INSTANCE, FieldValueResolver.INSTANCE, MethodValueResolver.INSTANCE)
                 .build();
@@ -37,10 +31,8 @@ public class HtmlReportRenderer implements ReportRenderer {
 
     @SneakyThrows
     private static Template compileTemplate(String location) {
-
         Handlebars handlebars = new Handlebars();
-//        handlebars.registerHelper("loud", (Helper<String>) (blog, options) -> options.fn(blog));
-
+        handlebars.registerHelper("sanitise", (Helper<String>) (input, options) -> sanitise(input));
         return handlebars.compile(location);
     }
 }
