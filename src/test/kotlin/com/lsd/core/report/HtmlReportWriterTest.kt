5d63393ac68c1aca0aa4bba2448549e0e3959134
nickmcdowall <nicholas.mcdowall@gmail.com>
2023-04-17 22:24:20 +0100

    feat: #75 Use CDN to serve css and js files
    User major version so that minor and patch versions are automatically used (not major versions to avoid breaking changes)

--- src/test/kotlin/com/lsd/core/report/HtmlReportWriterTest.kt
+++ src/test/kotlin/com/lsd/core/report/HtmlReportWriterTest.kt
@@ -9,9 +9,6 @@ import java.io.File
 internal class HtmlReportWriterTest {
 
     private val outputDirectory = File("build/reports/lsd/deeply/nested/directory")
-    private val customJsFile = File(outputDirectory, "custom.js")
-    private val styleCssFile = File(outputDirectory, "style.css")
-
     private val underTest = HtmlReportWriter(HtmlReportRenderer())
     private val aReport = Report("report title")
 
@@ -25,25 +22,5 @@ internal class HtmlReportWriterTest {
         val reportFile = underTest.writeToFile(report = aReport, outputDir = outputDirectory).toFile()
 
         assertThat(reportFile).exists()
-        assertThat(customJsFile).exists()
-        assertThat(styleCssFile).exists()
-        assertThat(reportFile.readText())
-            .contains("""<link rel="stylesheet" href="style.css">""")
-            .contains("""<script src="custom.js"></script>""")
-    }
-
-    @Test
-    fun staticFilesNotAddedWhenLocalForStaticContentIsFalse() {
-        val reportFile = underTest.writeToFile(
-            report = aReport.copy(useLocalStaticFiles = false),
-            outputDir = outputDirectory
-        ).toFile()
-
-        assertThat(reportFile).exists()
-        assertThat(styleCssFile).doesNotExist()
-        assertThat(customJsFile).doesNotExist()
-        assertThat(reportFile.readText())
-            .contains("""<link rel="stylesheet" href="https://lsd-consulting.github.io/lsd-core/src/main/resources/static/style.css">""")
-            .contains("""<script src="https://lsd-consulting.github.io/lsd-core/src/main/resources/static/custom.js"></script>""")
     }
 }
