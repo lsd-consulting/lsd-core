6c44a373f9cfd4c47904c9249514215daf0c40d5
nickmcdowall <nicholas.mcdowall@gmail.com>
2023-04-30 14:22:32 +0100

    chore: refactor index ui test

--- src/test/kotlin/com/lsd/core/IndexUiTest.kt
+++ src/test/kotlin/com/lsd/core/IndexUiTest.kt
@@ -17,45 +17,58 @@ class IndexUiTest {
 
     @Test
     fun createsIndex() {
+        givenAReportWithSuccessWarnAndErrorScenarios(title = "First Report")
+        andAReportWithSuccessAndWarnScenarios(title = "Second Report")
+        andAReportWithSuccessScenario(title = "Third Report")
+        andAReportWithNoScenarios(title = "Fourth Report")
+
+        whenTheIndexIsGenerated()
+
+        thenTheIndexPageContainsLinksToAllReports()
+    }
+
+    private fun givenAReportWithSuccessWarnAndErrorScenarios(title: String) {
         lsd.completeScenario("Scenario 1", "Error", Status.ERROR)
         lsd.completeScenario("Scenario 2", "Warn", Status.FAILURE)
         lsd.completeScenario("Scenario 3", "Success", Status.SUCCESS)
-        lsd.completeReport("First Report (Error)")
+        lsd.completeReport(title)
+    }
+
+    private fun andAReportWithNoScenarios(title: String) {
+        lsd.completeReport(title)
+    }
+
+    private fun andAReportWithSuccessScenario(title: String) {
+        lsd.completeScenario("Scenario 1", "Success", Status.SUCCESS)
+        lsd.completeReport(title)
+    }
+
+    private fun andAReportWithSuccessAndWarnScenarios(title: String) {
         lsd.completeScenario("Scenario 1", "Warn", Status.FAILURE)
         lsd.completeScenario("Scenario 2", "Success", Status.SUCCESS)
-        lsd.completeReport("Second Report (Warn)")
-        lsd.completeScenario("Scenario 1", "Success", Status.SUCCESS)
-        lsd.completeReport("Third Report")
-        lsd.completeReport("Fourth Report")
+        lsd.completeReport(title)
+    }
 
+    private fun whenTheIndexIsGenerated() {
         page.navigate("file://${lsd.createIndex().absolute()}")
+    }
 
+    private fun thenTheIndexPageContainsLinksToAllReports() {
         assertThat(page).hasTitle("Index")
         assertThat(page.locator("h1.logo")).isVisible()
         assertThat(page.locator("label.index")).hasText("Available Reports")
 
-        page.getByText("First Report (Error)").let {
-            assertThat(it).isVisible()
-            assertThat(it).hasClass("error")
-            assertThat(it).hasAttribute("href", "FirstReport(error).html")
-        }
+        assertHyperlinkCreated(linkText = "First Report", cssClass = "error", href = "FirstReport.html")
+        assertHyperlinkCreated(linkText = "Second Report", cssClass = "warn", href = "SecondReport.html")
+        assertHyperlinkCreated(linkText = "Third Report", cssClass = "success", href = "ThirdReport.html")
+        assertHyperlinkCreated(linkText = "Fourth Report", cssClass = "", href = "FourthReport.html")
+    }
 
-        page.getByText("Second Report (Warn)").let {
+    private fun assertHyperlinkCreated(linkText: String, cssClass: String, href: String) {
+        page.getByText(linkText).let {
             assertThat(it).isVisible()
-            assertThat(it).hasClass("warn")
-            assertThat(it).hasAttribute("href", "SecondReport(warn).html")
-        }
-
-        page.getByText("Third Report").let {
-            assertThat(it).isVisible()
-            assertThat(it).hasClass("success")
-            assertThat(it).hasAttribute("href", "ThirdReport.html")
-        }
-
-        page.getByText("Fourth Report").let {
-            assertThat(it).isVisible()
-            assertThat(it).hasClass("")
-            assertThat(it).hasAttribute("href", "FourthReport.html")
+            assertThat(it).hasClass(cssClass)
+            assertThat(it).hasAttribute("href", href)
         }
     }
 }
