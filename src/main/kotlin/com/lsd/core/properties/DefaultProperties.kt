5394f0f56bc0b357d60d7454fa8ceb8643ee9a36
nickmcdowall <nicholas.mcdowall@gmail.com>
2023-05-01 15:58:17 +0100

    fix: bump CDN version to new major version

--- src/main/kotlin/com/lsd/core/properties/DefaultProperties.kt
+++ src/main/kotlin/com/lsd/core/properties/DefaultProperties.kt
@@ -11,7 +11,7 @@ object DefaultProperties {
     fun defaultProperties(): Properties {
         return Properties().apply {
             setProperty(LABEL_MAX_WIDTH, "200")
-            setProperty(DIAGRAM_THEME, "lsd-light from https://cdn.jsdelivr.net/gh/lsd-consulting/lsd-core@4/src/main/resources/static/")
+            setProperty(DIAGRAM_THEME, "lsd-light from https://cdn.jsdelivr.net/gh/lsd-consulting/lsd-core@5/src/main/resources/static/")
             setProperty(OUTPUT_DIR, "build/reports/lsd")
             setProperty(DETERMINISTIC_IDS, "false")
             setProperty(MAX_EVENTS_PER_DIAGRAM, "50")
